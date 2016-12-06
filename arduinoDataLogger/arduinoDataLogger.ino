1192042fdfbd3ebd2cde6f5a76d97d4087761192
Halie Murray-Davis <hmurraydavis@yahoo.com>
2016-12-06 14:00:55 +0000

    Use set frequency of data collection for compatibility with FFT.

--- arduinoDataLogger/arduinoDataLogger.ino
+++ arduinoDataLogger/arduinoDataLogger.ino
@@ -1,3 +1,5 @@
+double perviousTime = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -7,20 +9,23 @@ void loop()
 {
   while (1) 
   {
-    float sensorValueX = analogRead(A0);
-    float sensorValueY = analogRead(A1);
-    float sensorValueZ = analogRead(A2);
-
-    Serial.print("$T:");
-    Serial.print( millis() );
-    Serial.print(",X:");
-    Serial.print( sensorValueX );
-    Serial.print(",Y:");
-    Serial.print( sensorValueY );
-    Serial.print( ",Z:" );
-    Serial.print( sensorValueZ );
-    Serial.println(); 
-    //delay(1);
+    if( (millis() - perviousTime) >= 5){
+      perviousTime = millis();
+      float sensorValueX = analogRead(A0);
+      float sensorValueY = analogRead(A1);
+      float sensorValueZ = analogRead(A2);
+  
+      Serial.print("$T:");
+      Serial.print( perviousTime );
+      Serial.print(",X:");
+      Serial.print( sensorValueX );
+      Serial.print(",Y:");
+      Serial.print( sensorValueY );
+      Serial.print( ",Z:" );
+      Serial.print( sensorValueZ );
+      Serial.println(); 
+      //delay(1);
+    }
   }
   
 }
